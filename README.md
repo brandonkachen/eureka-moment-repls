9f5acfc90ef98494ac3387808b7c98f2745e161b
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-05-08 04:27:54 -0700

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # eureka-moment-repls
+
+Welcome to your pretest!
+
+To get started, hit the `Files` icon on the upper left and switch to `main.cs`. It will contain further instructions for this assessment.
--- main.cs
+++ main.cs
@@ -10,6 +10,14 @@ public class MergeNames
 
   public static void Main(string[] args)
   {
+    /* Problem #1
+    Implement the UniqueNames method. 
+    When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. 
+    The returned array should have no duplicates.
+
+    For example, calling MergeNames.UniqueNames(new string[]{'Ava', 'Emma', 'Olivia'}, new string[]{'Olivia', 'Sophia', 'Emma'}) 
+    should return an array containing Ava, Emma, Olivia, and Sophia in any order.
+    */
     string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
     string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
     Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
