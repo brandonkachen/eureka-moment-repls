ee219f4bfbeddc723f8656e50ae51e202fcd3e48
brandon chen <brandon@thementorshipproject.us>
2020-05-25 06:37:40 +0000

    added example program containing syntax useful for getting started

--- mini_rpg.cpp
+++ mini_rpg.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-  std::cout << "Hello World!\n";
+  // Create a string. Note that it was imported at the top
+  std::string welcome_message = "Welcome, please enter your name: ";
+  std::cout << welcome_message;
+
+  std::string player_name = "Player";
+  // Read in a value the user typed into the command line
+  std::cin >> player_name;
+
+  // print to the console
+  std::cout << "Welcome, player " + player_name << std::endl;
 }
---
+++ syntax.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Single-line comment
+
+/* Multi-line comments!
+Useful for explaining stuff
+in detail. */
+
+
+int main()
+{
+
+  // Integer objects are declared like this
+  int i = 0;
+
+  // Other fundamental data types: 
+  float f = 0.0;
+  double d = 0.0; // more precise than float
+  bool b = true;
+  char c = "x"; // single character (ASCII standard by default)
+
+  // Re-assigning values
+
+  
+  /* 
+
+  By convention, returning 0 means success and 1 means failure.
+  Programs running your code may do something different depending
+  on this return code.
+
+  */
+  return i;
+}
