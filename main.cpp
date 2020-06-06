f11c7e474a8f47289a1d70bc200da054ee2f32eb
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-06-05 23:39:07 -0700

    added some instructions in code

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  // std::cout << "Hello World!\n";
+
+  // Task 1
+
+  std::string item1_name = "";        // TODO: fill in
+  std::string item1_description = ""; // TODO: fill in
+  std::string item1_effect = "";      // TODO: fill in
+  std::string item1_rarity = "";      // TODO: fill in
+
+  // Task 2
+
+  // TODO: add all the attributes.
+  std::string item2_name = "health_potion";
+
+  // Task 3
+
+  // Task 4
+
+  // Task 5
+
+  // Task 6
+
+  // Task 7
+
+  // Task 8
+
+  HealthPotion hp;
+  int turn = 0;
+  while (turn < 3)
+  {
+    std::cout << "Turn #" << turn << std::endl;
+
+    // Decrement the timer
+  }
+
+  // Task 9
+
+  // Task 10
+
+  HealthPotion hp10;
+  while (turn < 3)
+  {
+    std::cout << "Turn #" << turn << std::endl;
+
+    hp10.cleanup(); // called after every turn
+  }
+}
