98b623c7c9f35f65808212edfa0df6ff1d1ff8d1
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-07-14 01:55:39 -0700

    updated lesson instructions (50cba45e8b58bf5a2b6c60dd43d0db75688a6a77)

--- main.cpp
+++ main.cpp
@@ -12,12 +12,10 @@ int main()
   std::string item1_effect = "";      // TODO: fill in
   std::string item1_rarity = "";      // TODO: fill in
 
-  // Task 2
+  // Task 2 - 3
 
   // TODO: add all the attributes.
   std::string item2_name = "health_potion";
-
-  // Task 3
 
   // Task 4
 
@@ -27,26 +25,29 @@ int main()
 
   // Task 7
 
-  // Task 8
+  // Task 8 - 11 (uncomment the following code when you're ready to start)
 
-  HealthPotion hp;
-  int turn = 0;
-  while (turn < 3)
-  {
-    std::cout << "Turn #" << turn << std::endl;
+  // HealthPotion hp;
+  // int turn = 0;
+  // while (turn < 3)
+  // {
+  //   std::cout << "Turn #" << turn << std::endl;
 
-    // Decrement the timer
-  }
+  //   // Decrement the timer
+  // }
 
-  // Task 9
+  // Task 12 (uncomment the following code when you're ready to start)
 
-  // Task 10
+  // HealthPotion hp10;
+  // while (turn < 3)
+  // {
+  //   std::cout << "Turn #" << turn << std::endl;
 
-  HealthPotion hp10;
-  while (turn < 3)
-  {
-    std::cout << "Turn #" << turn << std::endl;
+  //   if hp10.shouldCleanup() {
+  //     delete hp10;
+  //     break;
+  //   };
+  // }
 
-    hp10.cleanup(); // called after every turn
-  }
+  // Task 13
 }
